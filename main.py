7558b5cd1d1782c013b243fc3d7d3b3eb228f669
glennhergendi <glenn.nh100@gmail.com>
2023-09-20 19:18:10 +0700

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 class LuasBalok:
     def __init__(self, panjang, lebar):
-        self.panjang= 10
-        self.lebar= 5
-Balok= LuasBalok(10,5)
-print('-- Mencari luas balok --')
-print('panjang balok :',Balok.panjang)
-print('lebar balok :',Balok.lebar)
-print('luas balok :',Balok.panjang * Balok.lebar)
+        self.panjang = 10
+        self.lebar = 5
+
+
+Balok = LuasBalok(10, 5)
+print("-- Mencari luas balok --")
+print("panjang balok :", Balok.panjang)
+print("lebar balok :", Balok.lebar)
+print("luas balok :", Balok.panjang * Balok.lebar)
+# tes
